eac99e333a63bb4fc17f3d452e8fcb1bcbd035e3
ChambaG <salvador.gutierrez2k@me.com>
2018-11-07 15:36:47 -0600

    work 11/07/18

--- WebScraping.py
+++ WebScraping.py
@@ -14,53 +14,27 @@ def get_auction(url):
 aucLocation_url = 'https://www.iaai.com/locations'
 context = ssl._create_unverified_context()
 uClient = uReq(aucLocation_url, context=context)
+print("Loading...")
 page_html = uClient.read()
+print("Loading...")
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
+print("Loading...")
 locations = page_soup.findAll("a", {"class":"detailsLink"})
-
+print("Loading...")
 locations_file = open('iaai_locations.txt', 'w+')
 for i in range(0, len(locations)):
     locations_file.write(str(locations[i]) + "\n")
+    print("Loading for loop... " + str(i) + " out of " + str(len(locations)))
     html = str(locations[i])
     location_url = 'https://iaai.com'
     x = 29
     doubleQ = '"'
     while html[x] != doubleQ:
         location_url += html[x]
+        print("Loading while..." + str(x) + " out of " + str(len(locations)))
         x += 1
-
+    print(location_url)
+    print("Get Auction for " + str(i))
     get_auction(location_url)
-
-
-
-
-
-    new_soup = soup(new_html, "html.parser")
-
-
-aucLocation_url = 'https://www.iaai.com/locations'
-context = ssl._create_unverified_context()
-uClient = uReq(aucLocation_url, context=context)
-page_html = uClient.read()
-uClient.close()
-
-page_soup = soup(page_html, "html.parser")
-locations = page_soup.findAll("a", {"class":"detailsLink"})
-
-locations_file = open('iaai_locations.txt', 'w+')
-for i in range(0, len(locations)):
-    locations_file.write(str(locations[i]) + "\n")
-    html = str(locations[i])
-    location_url = 'https://iaai.com'
-    x = 29
-    doubleQ = '"'
-    while html[x] != doubleQ:
-        location_url += html[x]
-        x += 1
-
-    get_auction(location_url)
-
-
-
