4bbaa0c9174896f6098de61a48117d6aee34a873
ChambaG <salvador.gutierrez2k@me.com>
2019-01-22 17:16:03 -0600

    22/Jan/2019

--- CarClass.py
+++ CarClass.py
@@ -1,8 +1,13 @@
 class Car:
 
-    def __init__(self, year):
-        self.year = year
+    def __init__(self):
+        self.year = 0
         self.make = "Default"
         self.model = ""
         self.damage = ""
-        self.bid = ""
+        self.bid = 0
+        self.image = ""
+        self.url = ""
+
+    def show(self):
+        print(str(self.year) + " " + self.make + " " + self.damage + " " + str(self.bid) + " " + self.damage + "\n" + self.url)
--- IAAI/iaai_Scraping.py
+++ IAAI/iaai_Scraping.py
@@ -5,94 +5,6 @@ from CarClass import Car
 import datetime
 context = ssl._create_unverified_context()
 
-car_list = []
-
-
-def get_cars(url):
-    newclient = uReq(url, context= context)
-    new_html = newclient.read()
-    print(new_html)
-    newclient.close()
-    new_soup = soup(new_html, "html.parser")
-
-    # Scrape the year
-    car_year_soup = new_soup.findAll("td", {"class": "info year"})
-    indexes = []
-    previous_list = len(car_list)
-    for i in range(0, len(car_year_soup)):
-        year = ""
-        strng = str(car_year_soup[i])
-        for x in range(24, 29):
-            year += str(strng[x])
-        print(strng + " || " + year)
-        if int(year) >= 2016:
-            indexes.append(i)
-            car_list.append(Car(year))
-
-    print("There are " + str(len(indexes)) + " qualifying vehicles in this auction")
-
-    # Scrape the make
-    car_make_soup = new_soup.findAll("td", {"class": "info make"})
-    c = 0
-    while c < len(indexes):
-        make = ""
-        strng = str(car_make_soup[indexes[c]])
-        x = 24
-        while strng[x] != "<":
-            make += strng[x]
-            x += 1
-        print(strng + " || " + make)
-        car_list[c + previous_list].make = make
-        c += 1
-
-    # Scrape the model
-    car_model_soup = new_soup.findAll("td", {"class": "info model"})
-    c = 0
-    while c < len(indexes):
-        model = ""
-        strng = str(car_model_soup[indexes[c]])
-        x = 24
-        while strng[x] != "<":
-            model += strng[x]
-            x += 1
-        print(strng + " || " + model)
-        car_list[c + previous_list].model = model
-        c += 1
-
-    # Scrape the damage
-
-
-def get_auction(url):
-    newclient = uReq(url, context=context)
-    new_html = str(newclient.read())
-    newclient.close()
-
-    new_soup = soup(new_html, "html.parser")
-    auction_this_day = new_soup.findAll("a", {"class":"details-auc-date"})
-    print("There are " + str(len(auction_this_day)) + " auctions in this location")
-    html = str(auction_this_day)
-    date = ""
-    for x in range(95, 103):
-        date += html[x]
-    print(date)
-    if check_date(date):
-        locations_file = open('iaai_location.txt', 'w+')
-        for i in range(0, len(auction_this_day)):
-            locations_file.write(str(auction_this_day[i]) + "\n")
-            html = str(auction_this_day[i])
-            location_url = 'https://iaai.com'
-            x = 34
-            doubleq = '"'
-            while html[x] != doubleq:
-                location_url += html[x]
-                x += 1
-            print(location_url)
-            print("Get Auction for " + str(i))
-
-            get_cars(url)
-    else:
-        print("This auction was omitted")
-
 
 def initiate():
     aucLocation_url = 'https://www.iaai.com/locations'
@@ -125,6 +37,138 @@ def initiate():
         get_auction(location_url)
 
 
+def get_auction(url):
+    newclient = uReq(url, context=context)
+    new_html = str(newclient.read())
+    newclient.close()
+
+    new_soup = soup(new_html, "html.parser")
+    auction_this_day = new_soup.findAll("a", {"class":"details-auc-date"})
+    print("There are " + str(len(auction_this_day)) + " auctions in this location")
+    html = str(auction_this_day)
+    locations_file = open('iaai_location.txt', 'w+')
+    for i in range(0, len(auction_this_day)):
+        date = ""
+        locations_file.write(str(auction_this_day[i]) + "\n")
+        html = str(auction_this_day[i])
+        location_url = 'https://iaai.com'
+        x = 34
+        doubleq = '"'
+        while html[x] != doubleq:
+            location_url += html[x]
+            x += 1
+        for y in range(76, 84):
+            date += location_url[y]
+        print(location_url)
+        if check_date(date):
+            print(date)
+            print("Get Cars from auction #" + str(i + 1) + " out of " + str(len(auction_this_day)))
+            get_cars(location_url)
+
+
+def get_cars(url):
+    newclient = uReq(url, context= context)
+    new_html = newclient.read()
+    newclient.close()
+    new_soup = soup(new_html, "html.parser")
+
+    car_year_soup = new_soup.findAll("td", {"class": "info year"})
+    indexes = []
+    for i in range(0, len(car_year_soup)):
+        year = ""
+        strng = str(car_year_soup[i])
+        for x in range(24, 29):
+            year += str(strng[x])
+        if int(year) >= 2016:
+            indexes.append(i)
+
+    print("There are " + str(len(indexes)) + " qualifying vehicles in this auction")
+    car_list = [Car() for x in range(len(indexes))]
+
+    # Scrape the year
+    i = 0
+    while i < len(indexes):
+        year = ""
+        strng = str(car_year_soup[indexes[i]])
+        for x in range(24, 29):
+            year += str(strng[x])
+        car_list[i].year = int(year)
+        i += 1
+
+    # Scrape the make
+    car_make_soup = new_soup.findAll("td", {"class": "info make"})
+    c = 0
+    while c < len(indexes):
+        make = ""
+        strng = str(car_make_soup[indexes[c]])
+        x = 24
+        while strng[x] != "<":
+            make += strng[x]
+            x += 1
+        make = "".join(make.splitlines())
+        car_list[c].make = make
+        c += 1
+
+    # Scrape the model
+    car_model_soup = new_soup.findAll("td", {"class": "info model"})
+    c = 0
+    while c < len(indexes):
+        model = ""
+        strng = str(car_model_soup[indexes[c]])
+        x = 24
+        while strng[x] != "<":
+            model += strng[x]
+            x += 1
+        model = "".join(model.splitlines())
+        car_list[c].model = model
+        c += 1
+
+    # Get the link of the vehicle to scrape additional information
+    car_link = new_soup.findAll("td", {"class": "info secondCol stockNbr"})
+    for i in range(0, len(indexes)):
+        car_url = "https://www.iaai.com"
+        code = str(car_link[indexes[i]])
+        for x in range(92, 133):
+            car_url += code[x]
+        if car_url.find("/Vehicle?") == -1:
+            car_url = "https://www.iaai.com"
+            for y in range(179, 220):
+                car_url += code[y]
+        car_list[i].url = cleanup(car_url)
+
+    # Scrape the Bid and Damage
+    for i in range(0, len(indexes)):
+        newer_client = uReq(car_list[i].url, context=context)
+        newer_html = newer_client.read()
+        newer_client.close()
+        souping = soup(newer_html, "html.parser")
+
+        # Scraping the bid
+        bid_soup = souping.findAll("span", {"class": "high-price"})
+        bid_soup_str = str(bid_soup)
+        bid = ""
+        c = 27
+        while c < len(bid_soup_str) and bid_soup_str[c] != "<":
+            bid += bid_soup_str[c]
+            c += 1
+        bid = bid.replace(',', '')
+        car_list[i].bid = int(bid)
+
+        # Scraping the damage
+        damage_soup = souping.findAll("div", {"class": "col-7 col-value flex-self-end"})
+        damage_soup_str = str(damage_soup[1])
+        damage = ""
+        c = 54
+        while c < len(damage_soup_str) and damage_soup_str[c] != "<":
+            if damage_soup_str[c] == " " and damage_soup_str[c + 1] == "<":
+                c = len(damage_soup_str)
+            else:
+                damage += damage_soup_str[c]
+                c += 1
+        damage = damage.replace("&amp;", "&")
+        car_list[i].damage = damage
+
+
 def check_date(date):
 
     on_bound = True
@@ -133,36 +177,44 @@ def check_date(date):
     day = date[2] + date[3]
     year = date[4] + date[5] + date[6] + date[7]
 
-    week = now.date().isocalendar()[1] + 2
+    week = now.date().isocalendar()[1] + 1
 
     d = str(now.year) + "-W" + str(week)
+    # Limit date
     r = datetime.datetime.strptime(d + '-0', "%Y-W%W-%w")
-    print(r.month)
-    print(month)
-
-    if r.month >= int(month) and on_bound:
-        print("yes")
+    print(r)
 
     if r.year == int(year):
-        print(str(r.year) + " | " + year)
         on_bound = True
-        if r.month >= int(month) and on_bound:
-            print(str(r.month) + " | " + month)
+        if r.month == int(month) and on_bound:
             on_bound = True
             if 1 <= int(day) <= r.day and on_bound:
-                print(str(r.day) + " | " + day)
                 on_bound = True
+                print("Date on bound")
             else:
+                print("Date not in bound")
                 on_bound = False
         else:
-            on_bound = False
+            if r.month > int(month):
+                on_bound = True
+                print("Date on Bound")
+            else:
+                print("Date not in bound")
+                on_bound = False
     else:
+        print("Date not in bound")
         on_bound = False
-
-    print("Auction in is in bound")
     return on_bound
 
 
-initiate()
+def cleanup(url):
+    url = url.replace("com.", "com")
+    url = url.replace(".Vehicle", "/Vehicle")
+    url = url.replace("mVehicle", "m/Vehicle")
+
+    return url
+
+
+# initiate()
 # get_auction("https://www.iaai.com/locations/140/albuquerque")
-# get_cars("https://www.iaai.com/Auctions/BranchListingView.aspx?branchCode=711&aucDate=12282018")
+get_cars("https://iaai.com/Auctions/BranchListingView.aspx?branchCode=438&amp;aucDate=01242019")
